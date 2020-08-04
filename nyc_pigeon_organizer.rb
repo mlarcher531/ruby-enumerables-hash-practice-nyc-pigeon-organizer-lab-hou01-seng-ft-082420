f43f96eb64c116eba57656abe2eba2cdbec67722
Mike Archer <mlarcher531@gmail.com>
2020-08-04 16:20:30 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -2,7 +2,7 @@ require 'pry'
 
 def nyc_pigeon_organizer(data)
   final_result = data.each_with_object({}) do |(key, value), final_array|
-    #binding.pry
+    binding.pry
     value.each do |inner_key, names|
       #binding.pry
       names.each do |name|
